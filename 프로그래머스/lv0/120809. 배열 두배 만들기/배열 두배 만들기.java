2bff89c821db729e6c2a1976ca8909f7640d260e
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-13 20:44:35 +0900

    [level 0] Title: 배열 두배 만들기, Time: 2.22 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
+++ 프로그래머스/lv0/120809. 배열 두배 만들기/배열 두배 만들기.java
@@ -1,12 +1,4 @@
 class Solution {
-    // public int[] solution(int[] numbers) {
-    //     int[] answer = numbers;
-    //     for(int i = 0; i < numbers.length; i++) {
-    //         answer[i] = numbers[i] * 2;
-    //     }
-    //     return answer;
-    // }
-    
     public int[] solution(int[] numbers) {
         return java.util.Arrays.stream(numbers).map(i -> i * 2).toArray();
     }
