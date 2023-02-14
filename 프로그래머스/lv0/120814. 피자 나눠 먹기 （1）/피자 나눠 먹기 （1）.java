997a0e292465066f32326e81ce1a15cab2abb495
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-15 00:34:16 +0900

    [level 0] Title: 피자 나눠 먹기 (1), Time: 0.01 ms, Memory: 71 MB -BaekjoonHub

--- 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
+++ 프로그래머스/lv0/120814. 피자 나눠 먹기 （1）/피자 나눠 먹기 （1）.java
@@ -2,8 +2,10 @@ class Solution {
     public int solution(int n) {
         int answer = 0;
         if(n <= 7) answer = 1;
-        else if(n % 7 == 0) answer = n / 7;
-        else answer = n / 7 + 1;
+        else if(n%7 == 0) answer = n/7;
+        else answer = n/7 + 1;
         return answer;
+        // return n%7 == 0 ? n/7 : n/7 + 1;
+        // return (n + 6) / 7;
     }
 }
