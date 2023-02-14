3ed6367ad5a9ae8b09ee043ad698087cb529a287
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-15 00:16:05 +0900

    [level 0] Title: 짝수는 싫어요, Time: 0.02 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
+++ 프로그래머스/lv0/120813. 짝수는 싫어요/짝수는 싫어요.java
@@ -1,6 +1,6 @@
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[n/2 + n%2];
+        int[] answer = new int[n / 2 + n % 2];
         for(int i = 0; i < answer.length; i++) {
             answer[i] = i * 2 + 1;
         }
