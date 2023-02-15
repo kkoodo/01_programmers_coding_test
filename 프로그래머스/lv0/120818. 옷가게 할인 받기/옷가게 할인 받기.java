a1be038983d8b57045485148068e407fb783a380
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-15 19:49:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -1,16 +1,15 @@
 class Solution {
     public int solution(int price) {
-        // double discount = 1.0;
-        // if(price < 100000) discount = 1;
-        // else if(100000 <= price && price < 300000) discount = 0.95;
-        // else if(300000 <= price && price < 500000) discount = 0.9;
-        // else discount = 0.8;
-        // return (int)(price * discount);
+        double discount = 1.0;
+        if(price < 100000) discount = 1;
+        else if(100000 <= price && price < 300000) discount = 0.95;
+        else if(300000 <= price && price < 500000) discount = 0.9;
+        else discount = 0.8;
+        return (int)(price * discount);
         
-        if(price >= 500000) price = (int)(price * 0.8);
-        else if(price >= 300000) price = (int)(price * 0.9);
-        else if(price >= 100000) price = (int)(price * 0.95);
-
-        return price;
+//         if(price >= 500000) price = (int)(price * 0.8);
+//         else if(price >= 300000) price = (int)(price * 0.9);
+//         else if(price >= 100000) price = (int)(price * 0.95);
+//         return price;
     }
 }
