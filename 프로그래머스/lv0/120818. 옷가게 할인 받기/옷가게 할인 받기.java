69a0d5c50455c6c96a66a5fd23f57d1d74bb9b7d
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-15 19:49:59 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.02 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
+++ 프로그래머스/lv0/120818. 옷가게 할인 받기/옷가게 할인 받기.java
@@ -6,7 +6,6 @@ class Solution {
         else if(300000 <= price && price < 500000) discount = 0.9;
         else discount = 0.8;
         return (int)(price * discount);
-        
 //         if(price >= 500000) price = (int)(price * 0.8);
 //         else if(price >= 300000) price = (int)(price * 0.9);
 //         else if(price >= 100000) price = (int)(price * 0.95);
