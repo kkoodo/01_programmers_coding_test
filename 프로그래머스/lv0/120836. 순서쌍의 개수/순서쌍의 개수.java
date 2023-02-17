05ca26e1bc832d8fe3130cd530e602aae3c4299a
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 03:41:40 +0900

    [level 0] Title: 순서쌍의 개수, Time: 18.19 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
+++ 프로그래머스/lv0/120836. 순서쌍의 개수/순서쌍의 개수.java
@@ -2,10 +2,10 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(int n) {
-        // int answer = 0;
-        // for(int i = n; 1 <= i; i--) {
-        //     if(n % i == 0) answer++;
-        // }
+        int answer = 0;
+        for(int i = n; 1 <= i; i--) {
+            if(n % i == 0) answer++;
+        }
         // return answer;
         return (int)IntStream.rangeClosed(1, n).filter(i -> n % i == 0).count();
     }
