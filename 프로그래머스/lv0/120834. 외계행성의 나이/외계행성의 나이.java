7a4ac0db7360906429a34518479126a193611efd
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 01:40:25 +0900

    [level 0] Title: 외계행성의 나이, Time: 9.29 ms, Memory: 83.6 MB -BaekjoonHub

--- 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
+++ 프로그래머스/lv0/120834. 외계행성의 나이/외계행성의 나이.java
@@ -1,3 +1,5 @@
+// import java.util.stream.*;
+
 class Solution {
     public String solution(int age) {
         String answer = "";
@@ -8,5 +10,13 @@ class Solution {
             answer += (char)(Integer.valueOf(arr[i]) + 97);
         }
         return answer;
+        // return String.valueOf(age).chars().mapToObj(operand -> String.valueOf((char) (49 + operand))).collect(Collectors.joining());
+        
+        // StringBuilder sb = new StringBuilder();
+        // while(age > 0) {
+        //     sb.insert(0, (char)((age % 10) + (int)'a'));
+        //     age /= 10;
+        // }
+        // return sb.toString();
     }
 }
