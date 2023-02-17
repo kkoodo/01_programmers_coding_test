4547a70245921313db204a8dd6dffd98677a8176
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 00:45:07 +0900

    [level 0] Title: 배열 자르기, Time: 0.01 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
+++ 프로그래머스/lv0/120833. 배열 자르기/배열 자르기.java
@@ -1,3 +1,6 @@
+// import java.util.*;
+// import java.util.stream.*;
+
 class Solution {
     public int[] solution(int[] numbers, int num1, int num2) {
         int[] answer = new int[num2 - num1 + 1];
@@ -5,5 +8,7 @@ class Solution {
             answer[i] = numbers[num1 + i];
         }
         return answer;
+        // return Arrays.copyOfRange(numbers, num1, num2 + 1);
+        // return IntStream.rangeClosed(num1, num2).map(i -> numbers[i]).toArray();
     }
 }
