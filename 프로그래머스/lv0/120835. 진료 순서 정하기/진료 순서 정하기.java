e578cb975bb6c6fde8f0251671a2a6409730f74b
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 03:24:45 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/lv0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -13,15 +13,15 @@ class Solution {
         return answer;
         /* emergency 배열을 map 돌릴 것임 */
         // return Arrays.stream(emergency).map(i -> Arrays
-        //                                                 .stream(emergency)  // emergency 배열 sort 하기 위한 작업	    
-        //                                                                     // ☞ cannot convert from IntStream to int
-        //                                                 .boxed()            // emergency 배열 sort 하기 위한 작업
-        //                                                                     // ☞ cannot convert from IntStream to int
-        //                                                 .sorted(Comparator.reverseOrder())  // 내림차순 정렬
-        //                                                                                     // ☞ cannot convert from Stream<Integer> to int
+        //                                                 .stream(emergency)  // ☞ cannot convert from IntStream to int
+        //                                                                     // emergency 배열 sort 하기 위한 작업
+        //                                                 .boxed()            // ☞ cannot convert from IntStream to int
+        //                                                                     // emergency 배열 sort 하기 위한 작업
+        //                                                 .sorted(Comparator.reverseOrder())  // ☞ cannot convert from Stream<Integer> to int
+        //                                                                                     // 내림차순 정렬
         //                                                                                     // [3, 76, 24] -> [76, 24, 3]
-        //                                                 .collect(Collectors.toList())       // 배열 정렬한거 리스트에 담기
-        //                                                                                     // ☞ cannot convert from List<Integer> to int
+        //                                                 .collect(Collectors.toList())       // ☞ cannot convert from List<Integer> to int
+        //                                                                                     // 정렬된 배열 리스트에 담기
         //                                                 .indexOf(i) + 1                     // 리스트에서 i 값을 지니는 요소의 index 값 + 1 을 i로 치환함
         //                                                                                     // i : 3 -> list에서 3 값의 index = 2 -> i : 2 + 1 = 3이 됨
         //                                    ).toArray();
