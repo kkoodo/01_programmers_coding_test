06ce2bb7627b667e9d6aeafb794824d8eb1d43d5
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-17 23:58:32 +0900

    [level 0] Title: 각도기, Time: 0.01 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120829. 각도기/각도기.java
+++ 프로그래머스/lv0/120829. 각도기/각도기.java
@@ -1,5 +1,5 @@
 class Solution {
     public int solution(int angle) {
-        return angle == 180 ? 4 : (angle < 90 ? 1 : (angle == 90 ? 2 : 3));
+        return angle == 180 ? 4 : angle < 90 ? 1 : angle == 90 ? 2 : 3;
     }
 }
