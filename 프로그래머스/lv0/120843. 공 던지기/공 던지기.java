1335d9f9b7a1dd99f3deee814c363e7ce611fe9a
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 23:58:14 +0900

    [level 0] Title: 공 던지기, Time: 0.08 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
+++ 프로그래머스/lv0/120843. 공 던지기/공 던지기.java
@@ -3,6 +3,6 @@ class Solution {
         int[] loop = new int[2 * k];
         for(int i = 0; i < loop.length; i++) loop[i] = numbers[i % numbers.length];
         return loop[2 * k - 2];
-        // return (k-1)*2 % numbers.length + 1;
+        // return (k - 1) * 2 % numbers.length + 1;
     }
 }
