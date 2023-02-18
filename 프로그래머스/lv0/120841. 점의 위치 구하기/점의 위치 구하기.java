14cd6a433a883fe02bdb46e514e066a9e4182b15
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 22:51:22 +0900

    [level 0] Title: 점의 위치 구하기, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
+++ 프로그래머스/lv0/120841. 점의 위치 구하기/점의 위치 구하기.java
@@ -1,5 +1,6 @@
 class Solution {
     public int solution(int[] dot) {
-        return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] < 0 ? 3 : 2;
+        return dot[0] > 0 ? dot[1] > 0 ? 1 : 4 : dot[1] > 0 ? 2 : 3;
+        // x좌표 > 0 ? y좌표 > 0 ? "둘 다 양수" : "y좌표 음수" : y좌표 > 0 ? "x좌표 음수" : "둘 다 음수";
     }
 }
