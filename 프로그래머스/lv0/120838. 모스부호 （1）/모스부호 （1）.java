4ff3850dea4f4188f9752b3d9fa0647836ec6a32
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-18 21:37:17 +0900

    [level 0] Title: 모스부호 (1), Time: 2.39 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String letter) {
+        String[] arr = letter.split(" ");
+        String answer = "";
+        Map<String, String> morse = new HashMap<String, String>() {
+            {
+                put(".-", "a");
+                put("-...", "b");
+                put("-.-.", "c");
+                put("-..", "d");
+                put(".", "e");
+                put("..-.", "f");
+                put("--.", "g");
+                put("....", "h");
+                put("..", "i");
+                put(".---", "j");
+                put("-.-", "k");
+                put(".-..", "l");
+                put("--", "m");
+                put("-.", "n");
+                put("---", "o");
+                put(".--.", "p");
+                put("--.-", "q");
+                put(".-.", "r");
+                put("...", "s");
+                put("-", "t");
+                put("..-", "u");
+                put("...-", "v");
+                put(".--", "w");
+                put("-..-", "x");
+                put("-.--", "y");
+                put("--..", "z"); 
+            }
+        };
+        
+        for(int i = 0; i < arr.length; i++) {
+            for(String key : morse.keySet()) {
+                if(arr[i].equals(key)) {
+                    answer += morse.get(key);
+                }
+            }
+        }
+        
+        return answer;
+    }
+}
