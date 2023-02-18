e96adc7122f3660b85054163f7782bfc202aebc9
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-02-19 00:05:53 +0900

    [level 0] Title: 배열 회전시키기, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.java
@@ -1,15 +1,6 @@
 class Solution {
     public int[] solution(int[] numbers, String direction) {
-        int[] answer = new int[numbers.length];
-        for(int i = 0; i < answer.length; i++) {
-            if(direction.equals("right")) {
-                if(i == 0) answer[i] = numbers[numbers.length - 1];
-                else answer[i] = numbers[i - 1];
-            } else {
-                if(i == answer.length - 1) answer[i] = numbers[0];
-                else answer[i] = numbers[i + 1];
-            }
-        }
+        int[] answer = {};
         return answer;
     }
 }
