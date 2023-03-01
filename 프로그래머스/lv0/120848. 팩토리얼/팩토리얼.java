cd942808680ebc1e8a4dd6e2c108abb2dcd71b05
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-02 02:02:42 +0900

    [level 0] Title: 팩토리얼, Time: 0.04 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
+++ 프로그래머스/lv0/120848. 팩토리얼/팩토리얼.java
@@ -1,10 +1,10 @@
 class Solution {
     public int solution(int n) {
         int answer = 1;
-        int count = 1;
-        while(count <= n) {
+        int factorial = 1;
+        while(factorial <= n) {
             answer++;
-            count *= answer;
+            factorial *= answer;
         }
         return answer - 1;
     }
