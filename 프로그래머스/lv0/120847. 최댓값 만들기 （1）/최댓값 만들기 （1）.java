084953894fa9da7bd730523bd9590ef46b4afa08
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-02 01:31:46 +0900

    [level 0] Title: 최댓값 만들기 (1), Time: 0.33 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
+++ 프로그래머스/lv0/120847. 최댓값 만들기 （1）/최댓값 만들기 （1）.java
@@ -2,9 +2,9 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] numbers) {
-        // Arrays.sort(numbers);
-        // return numbers[numbers.length - 1] * numbers[numbers.length - 2];
-        numbers = Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
-        return numbers[0] * numbers[1];
+        Arrays.sort(numbers);
+        return numbers[numbers.length - 1] * numbers[numbers.length - 2];
+        // numbers = Arrays.stream(numbers).boxed().sorted(Comparator.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        // return numbers[0] * numbers[1];
     }
 }
