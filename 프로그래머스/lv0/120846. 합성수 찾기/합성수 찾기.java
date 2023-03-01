872946db6760550694325e55b2e82b9dcfb307e1
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-02 01:21:57 +0900

    [level 0] Title: 합성수 찾기, Time: 0.02 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
+++ 프로그래머스/lv0/120846. 합성수 찾기/합성수 찾기.java
@@ -1,18 +1,14 @@
 class Solution {
     public int solution(int n) {
         int answer = 0;
-    for(int i = 2; i <= n; i++) {
-        for(int j = 2; j <= i; j++) {
-        	System.out.println("i : " + i);
-        	System.out.println("j : " + j);
-        	System.out.println(i % j);
-            if(i % j == 0 && i != j) {
-            	answer++;
-            	System.out.println("야호");
-            	break;
+        for(int i = n; 1 < i; i--) {
+            for(int j = i; 1 < j; j--) {
+                if(i % j == 0 && i != j) {
+                    answer++;
+                    break;
+                }
             }
         }
-    }
         return answer;
     }
 }
