b96382b42e3b419121af50c5ac331d9d0a04df7a
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-08 23:52:38 +0900

    [level 0] Title: 모음 제거, Time: 0.25 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.java
@@ -1,7 +1,7 @@
 class Solution {
     public String solution(String my_string) {
-        // return my_string.replaceAll("a|e|i|o|u", "");
-        return my_string.replaceAll("[aeiou]", "");
+        return my_string.replaceAll("a|e|i|o|u", "");
+        // return my_string.replaceAll("[aeiou]", "");
         // my_string.matches(".*[aeiou]+.*");
     }
 }
