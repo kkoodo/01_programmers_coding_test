eb7800ea9dbf5e674c7eefa2e6795a692d5c1dc1
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-09 00:09:50 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 2.37 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
+++ 프로그래머스/lv0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.java
@@ -6,5 +6,7 @@ class Solution {
         String[] array = my_string.replaceAll("[a-zA-Z]", "").split("");
         Arrays.sort(array);
         return Stream.of(array).mapToInt(Integer::parseInt).toArray();
+        // return Arrays.stream(array).mapToInt(e -> Integer.parseInt(e)).toArray();
+        // return Arrays.stream(my_string.replaceAll("[a-zA-Z]", "").split("")).sorted().mapToInt(Integer::parseInt).toArray();
     }
 }
