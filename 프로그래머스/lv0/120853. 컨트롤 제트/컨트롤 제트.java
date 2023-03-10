7e92ed39e858f3f960dc1a7d5b1248706924ac81
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-10 16:13:49 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.48 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
+++ 프로그래머스/lv0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -3,11 +3,18 @@ import java.util.*;
 class Solution {
     public int solution(String s) {
         int answer = 0;
-        String[] array = s.split(" ");
-        for(int i = 0; i < array.length; i++) {
-            if(!array[i].equals("Z")) answer += Integer.valueOf(array[i]);
-            else answer -= Integer.valueOf(array[i - 1]);
+        // String[] array = s.split(" ");
+        // for(int i = 0; i < array.length; i++) {
+        //     if(!array[i].equals("Z")) answer += Integer.valueOf(array[i]);
+        //     else answer -= Integer.valueOf(array[i - 1]);
+        // }
+        // return answer;
+        Stack<Integer> stack = new Stack<>();
+        for(String letter : s.split(" ")) {
+            if(letter.equals("Z")) stack.pop();
+            else stack.push(Integer.parseInt(letter));
         }
+        for(int i : stack) answer += i;
         return answer;
     }
 }
