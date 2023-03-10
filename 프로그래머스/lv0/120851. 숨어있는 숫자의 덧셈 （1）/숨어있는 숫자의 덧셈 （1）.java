9b47ea62916caaa8e4c68fa1629a6f42ad5ef7d8
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-10 13:44:09 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 2.19 ms, Memory: 71.3 MB -BaekjoonHub

--- 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -2,12 +2,12 @@ import java.util.stream.*;
 
 class Solution {
     public int solution(String my_string) {
-        // int answer = 0;
-        // int[] array = Stream.of(my_string.replaceAll("[a-zA-Z]", "").split("")).mapToInt(Integer::parseInt).toArray();
-        // for(int i = 0; i < array.length; i++) {
-        //     answer += array[i];
-        // }
-        // return answer;
-        return my_string.chars().mapToObj(i -> (char) i).filter(Character::isDigit).map(String::valueOf).mapToInt(Integer::valueOf).sum();
+        int answer = 0;
+        int[] array = Stream.of(my_string.replaceAll("[a-zA-Z]", "").split("")).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0; i < array.length; i++) {
+            answer += array[i];
+        }
+        return answer;
+        // return my_string.chars().mapToObj(i -> (char) i).filter(Character::isDigit).map(String::valueOf).mapToInt(Integer::valueOf).sum();
     }
 }
