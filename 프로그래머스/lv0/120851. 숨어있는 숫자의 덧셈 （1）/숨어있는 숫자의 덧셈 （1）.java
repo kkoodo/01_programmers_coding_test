155c037d9a76393e3c56e954aad51221c83ae945
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-10 13:39:43 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (1), Time: 1.97 ms, Memory: 73.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120851. 숨어있는 숫자의 덧셈 （1）/숨어있는 숫자의 덧셈 （1）.java
@@ -0,0 +1,12 @@
+import java.util.stream.*;
+
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        int[] array = Stream.of(my_string.replaceAll("[a-zA-Z]", "").split("")).mapToInt(Integer::parseInt).toArray();
+        for(int i = 0; i < array.length; i++) {
+            answer += array[i];
+        }
+        return answer;
+    }
+}
