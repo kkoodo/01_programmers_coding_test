4facc0f20f2436c2134e89e8aa038d6d5313c820
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-10 14:33:07 +0900

    [level 0] Title: 소인수분해, Time: 3.42 ms, Memory: 74.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120852. 소인수분해/소인수분해.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.util.stream.*;
+
+class Solution {
+    public int[] solution(int n) {
+        int[] div = new int[n];
+        for(int i = 2; i <= n; i++) {
+            if(n % i == 0) div[i - 1] = i;
+        }
+        
+        int[] sort = Arrays.stream(div).filter(idx -> idx != 0).toArray();
+        ArrayList<Integer> list = new ArrayList<>();
+        for(int i = 0; i < sort.length; i++) {
+            if(isPrime(sort[i])) list.add(sort[i]);
+        }
+        
+        int[] answer = list.stream().mapToInt(i -> i).toArray();
+        return answer;
+    }
+    
+    public boolean isPrime(int n) {
+        for(int i = 2; i <= (int)Math.sqrt(n); i++) {
+            if(n % i == 0) return false;
+        }
+        return true;
+    }
+}
