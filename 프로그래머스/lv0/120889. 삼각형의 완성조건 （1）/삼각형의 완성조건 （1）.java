5cd427d5ea183050d2d255754f8b4ee970885845
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-10 17:10:08 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.32 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -5,8 +5,8 @@ class Solution {
     public int solution(int[] sides) {
         Arrays.sort(sides);
         if(sides[0] + sides[1] > sides[2]) return 1;
-        // else return 2;
-        return 2;
+        else return 2;
+        // return 2;
         // return sides[0] + sides[1] > sides[2] ? 1 : 2;
         // return Arrays.stream(sides).max().getAsInt() < Arrays.stream(sides).sum() - Arrays.stream(sides).max().getAsInt() ? 1 : 2;
     }
