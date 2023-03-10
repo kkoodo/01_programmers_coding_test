4370cf2b826b70302c446866e21559b512562a3c
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-03-10 16:22:59 +0900

    [level 0] Title: 배열 원소의 길이, Time: 3.07 ms, Memory: 72 MB -BaekjoonHub

--- 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
+++ 프로그래머스/lv0/120854. 배열 원소의 길이/배열 원소의 길이.java
@@ -1,9 +1,12 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(String[] strlist) {
         int[] answer = new int[strlist.length];
         for(int i = 0; i < answer.length; i++) {
             answer[i] = strlist[i].length();
         }
-        return answer;
+        //return answer;
+        return Arrays.stream(strlist).mapToInt(String::length).toArray();
     }
 }
