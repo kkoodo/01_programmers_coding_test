fa68afb16bac71b24f42ed57edda14ce86cf1cfd
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-06-16 11:21:16 +0900

    [level 2] Title: 중성화 여부 파악하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
+++ 프로그래머스/lv2/59409. 중성화 여부 파악하기/중성화 여부 파악하기.sql
@@ -1,16 +1,15 @@
 -- 코드를 입력하세요
--- SELECT ANIMAL_ID
---      , NAME
---      , DECODE(SUBSTR(SEX_UPON_INTAKE, 0, 6), 'Intact', 'X', 'O')    AS "중성화"
---   FROM ANIMAL_INS
---  ORDER BY ANIMAL_ID;
- 
--- [다른풀이]
 SELECT ANIMAL_ID
      , NAME
-     , CASE WHEN INSTR(SEX_UPON_INTAKE, 'Intact') > 0 THEN 'X'
-            ELSE 'O' 
-            END AS "중성화"
+     , DECODE(SUBSTR(SEX_UPON_INTAKE, 0, 6), 'Intact', 'X', 'O')    AS "중성화"
   FROM ANIMAL_INS
- ORDER BY ANIMAL_ID;
+ ORDER BY ANIMAL_ID
  
+-- [다른풀이]
+-- SELECT ANIMAL_ID
+--      , NAME
+--      , CASE WHEN INSTR(SEX_UPON_INTAKE, 'Intact') > 0 THEN 'X'
+--             ELSE 'O' 
+--             END AS "중성화"
+--   FROM ANIMAL_INS
+--  ORDER BY ANIMAL_ID;
