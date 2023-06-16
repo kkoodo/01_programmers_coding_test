bdc1140a1056109965f2822425dfe69e694bbd70
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-06-16 09:25:16 +0900

    [level 2] Title: 동명 동물 수 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
+++ 프로그래머스/lv2/59041. 동명 동물 수 찾기/동명 동물 수 찾기.sql
@@ -1,14 +1,14 @@
 -- 코드를 입력하세요
   WITH TBL_COUNT
     AS (SELECT NAME
-             , COUNT(NAME)  AS "NM_CNT"
+             , COUNT(NAME)  AS "COUNT"
           FROM ANIMAL_INS
          GROUP BY NAME
        )
 SELECT NAME
-     , NM_CNT               AS "COUNT"
+     , COUNT
   FROM TBL_COUNT
- WHERE NM_CNT > 1
+ WHERE COUNT > 1
  ORDER BY NAME
 
 -- [다른풀이]
