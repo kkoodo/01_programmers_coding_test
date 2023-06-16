82bd68f1f21dcfe328be25ec7353f0589d8b734b
kkoodo <124335296+kkoodo@users.noreply.github.com>
2023-06-16 16:06:44 +0900

    [level 2] Title: 가격이 제일 비싼 식품의 정보 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
+++ 프로그래머스/lv2/131115. 가격이 제일 비싼 식품의 정보 출력하기/가격이 제일 비싼 식품의 정보 출력하기.sql
@@ -1,13 +1,25 @@
 -- 코드를 입력하세요
-  WITH TBL_ROWNUMBER
-    AS (SELECT PRODUCT_ID
-             , PRODUCT_NAME
-             , PRODUCT_CD
-             , CATEGORY
-             , PRICE
-          FROM FOOD_PRODUCT
-         ORDER BY PRICE DESC
-       )
+--   WITH TBL_ROWNUMBER
+--     AS (SELECT PRODUCT_ID
+--              , PRODUCT_NAME
+--              , PRODUCT_CD
+--              , CATEGORY
+--              , PRICE
+--           FROM FOOD_PRODUCT
+--          ORDER BY PRICE DESC
+--        )
+-- SELECT *
+--   FROM TBL_ROWNUMBER
+--  WHERE ROWNUM = 1;
+ 
+ 
+-- SELECT *
+-- FROM FOOD_PRODUCT
+-- WHERE PRICE IN (SELECT MAX(PRICE)
+--                FROM FOOD_PRODUCT)
+               
+-- [다른풀이]
 SELECT *
-  FROM TBL_ROWNUMBER
- WHERE ROWNUM = 1;
+  FROM FOOD_PRODUCT
+ ORDER BY PRICE DESC
+ FETCH FIRST 1 ROW ONLY;
